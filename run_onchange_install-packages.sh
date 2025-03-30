6666cbf9fd2590cf79b1988dda17ee41d90dd268
Erik Veld <erik.veld@gmail.com>
2025-03-30 13:48:07 +0200

    initial commit

---
+++ run_onchange_install-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+paru -S \
+	jq \
+	curl \
+	zip \
+	spotify \
+	librewolf-bin \
+	1password \
+	1password-cli \
+	goxlr-utility \
+	ghostty \
+	gnome-browser-connector \
+	gnome-shell-extensions \
+	steam
